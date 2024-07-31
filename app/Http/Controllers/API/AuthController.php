356eee80daba293d3e5e8f5c76a913368c87aa96
NaraMizaru <hilal.muhammad0807@gmail.com>
2024-07-31 09:42:32 +0700

    auth

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'fullname' => 'required|string',
+            'username' => 'required|string|min:2|max:12|unique:users,username',
+            'email' => 'required|email|string|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',
+            'number_phone' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Invalid fields',
+                'errors' => $validator->errors(),
+            ], 400);
+        }
+
+        $user = new User();
+        $user->fullname = $request->fullname;
+        $user->username = $request->username;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->number_phone = $request->number_phone;
+        $user->save();
+
+        if ($user->save()) {
+            $token = $user->createToken('api_token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'Register Successfully',
+                'token' => $token,
+                'user' => $user
+            ], 201);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'login' => 'required',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Invalid fields',
+                'errors' => $validator->errors(),
+            ], 400);
+        }
+
+        $login = $request->login;
+        $fieldType = filter_var($login, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+        $auth = Auth::attempt([$fieldType => $login, 'password' => $request->password]);
+        if ($auth) {
+            $token = $request->user()->createToken('api_token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'Login Successfully',
+                'token' => $token,
+                'user' => Auth::user()
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        
+    }
+}
