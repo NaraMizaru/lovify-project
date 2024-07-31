c732a3a8842b41f0589dae674e15dcbdd4b2f298
CRAAFFT <rifaasiraajuddin.123@gmail.com>
2024-07-31 09:21:29 +0700

    coba

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -58,4 +58,9 @@ class AuthController extends Controller
         Auth::logout();
         return redirect()->route('home');
     }
+
+    public function Halo()
+    {
+        return 'Halo';
+    }
 }
