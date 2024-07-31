72be933e33c1c9791d0aa2dbb19f11157dbc0d48
CRAAFFT <rifaasiraajuddin.123@gmail.com>
2024-07-31 11:16:26 +0700

    Buat fungsi Update Vendor dan Delete Vendor

--- app/Http/Controllers/Manage/VendorController.php
+++ app/Http/Controllers/Manage/VendorController.php
@@ -56,4 +56,54 @@ class VendorController extends Controller
 
         return redirect()->route('');
     }
+
+    public function updateVendor(Request $request, $id)
+    {
+        $vendor = Vendor::find($id);
+        $category = Category::find($vendor->category_id);
+
+        if ($category->name == 'venue') {
+            $credential = Validator::make($request->all(), [
+                'name' => ['required','string'],
+                'description' => ['required','string'],
+                'price' => ['required','integer'],
+                'address' => ['required','string'],
+                'total_guest' => ['required','integer'],
+            ]);
+        } else {
+            $credential = Validator::make($request->all(), [
+                'name' => ['required','string'],
+                'description' => ['required','string'],
+                'price' => ['required','integer'],
+            ]);
+        }
+
+        if ($credential->fails()) {
+            return redirect()->back()->withErrors($credential->errors())->withInput($request->all());
+        }
+        if ($category->name == 'venue') {
+            $vendor->name = $request->name;
+            $vendor->description = $request->description;
+            $vendor->price = $request->price;
+            $vendor->address = $request->address;
+            $vendor->total_guest = $request->total_guest;
+            $vendor->category_id = $category->id;
+            $vendor->save();
+        } else {
+            $vendor->name = $request->name;
+            $vendor->description = $request->description;
+            $vendor->price = $request->price;
+            $vendor->category_id = $category->id;
+            $vendor->save();
+        }
+
+        return redirect()->route('');
+    }
+
+    public function deleteVendor($id)
+    {
+        $vendor = Vendor::find($id);
+        $vendor->delete();
+        return redirect()->route('');
+    }
 }
