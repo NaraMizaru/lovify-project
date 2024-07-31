a6d1d0a388564f9a0bd66b17f39602a18446b70f
Ello17 <azzellofabian@gmail.com>
2024-07-31 14:01:09 +0700

    membuat page planning bervisual

--- resources/views/Template/Nav.blade.php
+++ resources/views/Template/Nav.blade.php
@@ -1,3 +1,15 @@
 <nav>
-
+<div class="logo"><h2>Lovify</h2></div>
+<div class="content">
+    <a href="#">Home</a>
+    <hr>
+    <a href="#">Packets</a>
+    <hr>
+    <a href="#">Vendors?</a>
+</div>
+<div class="account">
+    <a href="#">Login</a>
+    <hr>
+    <a href="#">Register</a>
+</div>
 </nav>
--- resources/views/Users/landingpage.blade.php
+++ resources/views/Users/landingpage.blade.php
@@ -4,5 +4,14 @@
 @endpush
 @section('title', 'Landing Page')
 @section('content')
-
+<div class="container">
+    <div class="box">
+        <div class="img">
+            <img src="{{asset('components/assets/transparant-black.png')}}" alt="">
+        </div>
+        <div class="button">
+            <a href="#" class="btn">Book Now</a>
+        </div>
+    </div>
+</div>
 @endsection
