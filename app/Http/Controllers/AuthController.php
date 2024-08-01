9d5481d5a5dbf1e4416f9341c29ad6304e8a0058
CRAAFFT <rifaasiraajuddin.123@gmail.com>
2024-08-01 08:02:47 +0700

    Ubah fungsi Register username nya diubah jadi unique

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,7 +13,7 @@ class AuthController extends Controller
     {
         $credential = Validator::make($request->all(), [
             'fullname' => ['required', 'string'],
-            'username' => ['required', 'string', 'min:2', 'max:12'],
+            'username' => ['required', 'string', 'min:2', 'max:12', 'unique:users,username'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'number_phone' => ['required', 'string'],
