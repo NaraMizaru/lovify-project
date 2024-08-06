74ad909af44b737a29fc0604665616562cdcca6e
Ello17 <azzellofabian@gmail.com>
2024-08-06 07:40:58 +0700

    -

--- resources/views/Template/sidebar.blade.php
+++ resources/views/Template/sidebar.blade.php
@@ -1,55 +1,52 @@
+<div class="navigation">
+    <ul>
+        <li>
+            <a href="#">
+                <img src="{{ asset('components/assets/L.png') }}" alt="" class="icon">
+                <span class="title">Lovify</span>
+            </a>
+        </li>        <hr color="white" style="display:flex; margin-right:10px; margin-bottom:10px;">
+        <li>
+            <a href="#">
+                <span class="icon"><i class="fa-solid fa-house"></i></span>
+                <span class="title">Dashboard</span>
+            </a>
+        </li>
 
-    <div class="navigation">
-        <ul>
-            <li>
-                <a href="#">
-                    <img src="{{asset('components/assets/L.png')}}" alt="" class="icon">
-                    <span class="title">Lovify</span>
-                </a>
-            </li>
+        <li>
+            <a href="{{ route('profile') }}">
+                <span class="icon"><i class="fa-solid fa-user"></i></span>
+                <span class="title">Profile</span>
+            </a>
+        </li>
 
-            <li>
-                <a href="#">
-                    <span class="icon"><i class="fa-solid fa-house"></i></span>
-                    <span class="title">Dashboard</span>
-                </a>
-            </li>
+        <li>
+            <a href="#">
+                <span class="icon"><i class="fa-duotone fa-solid fa-rings-wedding"></i></span>
+                <span class="title">Weddings</span>
+            </a>
+        </li>
 
-            <li>
-                <a href="{{route('profile')}}">
-                    <span class="icon"><i class="fa-solid fa-user"></i></span>
-                    <span class="title">Profile</span>
-                </a>
-            </li>
+        <li>
+            <a href="#">
+                <span class="icon"><i class="fa-solid fa-money-from-bracket"></i></span>
+                <span class="title">Transaction</span>
+            </a>
+        </li>
 
-            <li>
-                <a href="#">
-                    <span class="icon"><i class="fa-duotone fa-solid fa-rings-wedding"></i></span>
-                    <span class="title">Weddings</span>
-                </a>
-            </li>
+        <li>
+            <a href="#">
+                <span class="icon"><i class="fa-solid fa-clock-rotate-left"></i></span>
+                <span class="title">History</span>
+            </a>
+        </li>
 
-            <li>
-                <a href="#">
-                    <span class="icon"><i class="fa-solid fa-money-from-bracket"></i></span>
-                    <span class="title">Transaction</span>
-                </a>
-            </li>
-
-            <li>
-                <a href="#">
-                    <span class="icon"><i class="fa-solid fa-clock-rotate-left"></i></span>
-                    <span class="title">History</span>
-                </a>
-            </li>
-
-            <li>
-                <a href="#">
-                    <span class="icon"><i class="fa-solid fa-circle-info"></i></span>
-                    <span class="title">Help</span>
-                </a>
-            </li>
-        </ul>
-        <div class="toggle"></div>
-    </div>
-
+        <li>
+            <a href="#">
+                <span class="icon"><i class="fa-solid fa-circle-info"></i></span>
+                <span class="title">Help</span>
+            </a>
+        </li>
+    </ul>
+    <div class="toggle"></div>
+</div>
