9a0592c8f3a688e6642dca3994946e69ea49a3d1
KnowRise <rifaasiraajuddin.123@gmail.com>
2024-08-06 10:33:21 +0700

    -

--- resources/views/Users/home.blade.php
+++ resources/views/Users/home.blade.php
@@ -13,6 +13,11 @@
                 @foreach ($vendors as $vendor)
                     @if ($vendor->category_id == $category->id)
                         <div>
+                            @foreach ($attachments as $attachment)
+                                @if ($attachment->vendor_id == $vendor->id)
+                                    <img src="{{ asset($attachment->image_path) }}" alt="" width="200">
+                                @endif
+                            @endforeach
                             <h3>{{ $vendor->name }}</h3>
                         </div>
                     @endif
