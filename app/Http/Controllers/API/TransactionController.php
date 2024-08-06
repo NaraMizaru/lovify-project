3789996ab35fea978808773439cca7d4eefd6396
NaraMizaru <hilal.muhammad0807@gmail.com>
2024-08-06 12:23:05 +0700

    .

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TransactionController extends Controller
 {
     public function createTransaction(Request $request)
     {
-        
+        $validator = Validator::make($request->all(), [
+            'status' => 'required',
+            
+        ]);
     }
 }
