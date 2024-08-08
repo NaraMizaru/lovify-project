e7f6bf2a24f22764c29673ff4f66ef8bbf5b3d4f
KnowRise <rifaasiraajuddin.123@gmail.com>
2024-08-08 08:09:53 +0700

    -

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
@@ -11,21 +12,16 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $credential = Validator::make($request->all(), [
-            'fullname' => ['required','string'],
-            'username' => ['required','string', 'min:2', 'max:12'],
-            'email' => ['required','string','email','max:255', 'unique:users,email'],
-            'password' => ['required','string','min:8','confirmed'],
-            'number_phone' => ['required','string'],
-            'profile_image' => ['required', 'file', 'image', 'mimes:jpg, png, jpeg'],
+            'fullname' => ['required', 'string'],
+            'username' => ['required', 'string', 'min:2', 'max:12', 'unique:users,username'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'number_phone' => ['required', 'string'],
         ]);
 
-        if ($credential->fails())
-        {
+        if ($credential->fails()) {
             return redirect()->back()->withErrors($credential->errors())->withInput($request->all());
         }
-
-        $image = $request->file('profile_image');
-        $path = $image->store('profile_image' . $request->username, 'public');
 
         $user = new User();
         $user->fullname = $request->fullname;
@@ -33,6 +29,42 @@ class AuthController extends Controller
         $user->email = $request->email;
         $user->password = $request->fullname;
         $user->number_phone = $request->fullname;
-        $user->profile_image = $path;
+        $user->save();
+
+        Auth::login($user);
+        return redirect()->route('home');
+    }
+
+    public function login(Request $request)
+    {
+        $login = $request->login;
+        $type = filter_var($login, FILTER_VALIDATE_EMAIL);
+        if ($type) {
+            if (Auth::attempt(['email' => $request->login, 'password' => $request->password])) {
+                $user = Auth::user();
+                if ($user->role == 'admin') {
+                    return redirect()->route('home.admin');
+                }
+                return redirect()->route('home');
+            } else {
+                return redirect()->back()->with('message', 'Invalid Credentials');
+            }
+        } else {
+            if (Auth::attempt(['username' => $request->login, 'password' => $request->password])) {
+                $user = Auth::user();
+                if ($user->role == 'admin') {
+                    return redirect()->route('home.admin');
+                }
+                return redirect()->route('home');
+            } else {
+                return redirect()->back()->with('message', 'Invalid Credentials');
+            }
+        }
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('home');
     }
 }
