bf4b1bb0e195ebc56afe3ba64c9ed3f97f123d0f
KnowRise <rifaasiraajuddin.123@gmail.com>
2024-08-08 07:38:17 +0700

    -

--- resources/views/admin/vendor.blade.php
+++ resources/views/admin/vendor.blade.php
@@ -1,12 +1,16 @@
 @extends('layouts.app-admin')
 @push('css')
-    <link rel="stylesheet" href="{{ asset('components/css/admin/dasboard.css') }}">
-    <link rel="stylesheet" href="{{ asset('DataTables/datatables.min.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('components/css/admin/dasboard.css') }}"> --}}
+    {{-- <link rel="stylesheet" href="{{ asset('DataTables/datatables.css') }}"> --}}
+    {{-- <link rel="stylesheet" href="https://cdn.datatables.net/2.1.3/css/dataTables.dataTables.css" /> --}}
+    <link rel="stylesheet" href="//cdn.datatables.net/2.1.3/css/dataTables.dataTables.min.css">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> --}}
 @endpush
 @section('title', 'Admin')
 @section('content')
     <h1>Daftar Vendor</h1>
-    <table id="vendorTable" class="display" style="width:100%;">
+    <table id="myTable" class="display text-white" style="width:100%;">
         <thead>
             <tr>
                 <th>Nama</th>
@@ -20,6 +24,7 @@
                 <th>Nomor Rekening</th>
                 <th>Kategori</th>
                 <th>Detail</th>
+                <th>Aksi</th>
             </tr>
         </thead>
         <tbody>
@@ -57,17 +62,25 @@
             @endforeach
         </tbody>
     </table>
-    @push('js')
-        <script src="{{ asset('DataTables/datatables.min.js') }}"></script>
-        <script>
-            $(document).ready(function() {
-                $('#vendorTable').DataTable({
-                    responsive: true,
-                    language: {
-                        url: '//cdn.datatables.net/plug-ins/1.13.4/i18n/id.json'
-                    }
-                });
-            });
-        </script>
-    @endpush
 @endsection
+
+@push('js')
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="//cdn.datatables.net/2.1.3/js/dataTables.min.js"></script>
+    <script>
+        let table = new DataTable('#myTable');
+    </script>
+    {{-- <script src="{{ asset('DataTables/datatables.js') }}"></script>
+    <script src="https://cdn.datatables.net/2.1.3/js/dataTables.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#vendorTable').DataTable({
+                responsive: true,
+                language: {
+                    url: '//cdn.datatables.net/plug-ins/1.13.4/i18n/id.json'
+                }
+            });
+        });
+    </script> --}}
+@endpush
